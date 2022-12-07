97c0344b1222c0398bb01401c87c927e78df2aea
Yaldaba0th <43718531+Yaldaba0th@users.noreply.github.com>
2022-12-07 08:48:59 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -61,8 +61,6 @@ fetch('./gamelist/gdata.json')
 
 const f = document.getElementById('form');
 const q = document.getElementById('query');
-//const google = 'https://www.google.com/search?q=site%3A+';
-//const site = 'pagedart.com';
 
 function submitted(event) {
   //console.log({ data })
